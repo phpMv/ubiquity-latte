054e2593d0cd794fe51a03c88a06689b60da045b
jcheron <myaddressmail@gmail.com>
2023-01-16 03:11:44 +0100

    Update LatteTemplateGenerator.php

--- src/Ubiquity/views/engine/latte/LatteTemplateGenerator.php
+++ src/Ubiquity/views/engine/latte/LatteTemplateGenerator.php
@@ -13,19 +13,13 @@ namespace Ubiquity\views\engine\latte;
  */
 class LatteTemplateGenerator extends \Ubiquity\views\engine\TemplateGenerator {
 
-	private function preProcess(string $code): string {
-		$code=\preg_replace('@\|\s?raw@', '|noescape', $code);
-		return $code;
-	}
 
 	private function postProcess(string $code): string {
-		$code=\preg_replace('@\$(.*?)->(.*?)([\s|}]+)@', '\$$1["$2"]$3', $code);
-		return \str_replace(['$_self', '$nonce', '$config->siteUrl','{block _'], ['$this->getName()', '$nonce??""', "\$config['siteUrl']",'{block b_'], $code);
+		return \str_replace(['$_self', '$nonce', '{block _'], ['$this->getName()', '$nonce??""', '{block b_'], $code);
 	}
 
 	public function parseFromTwig(string $code): string {
 		if (\class_exists(\LatteTools\TwigConverter::class)) {
-			$code=$this->preProcess($code);
 			$converter = new \LatteTools\TwigConverter();
 			$code = $converter->convert($code);
 			return $this->postProcess($code);
