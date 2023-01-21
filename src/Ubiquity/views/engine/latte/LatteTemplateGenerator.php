aa41bf229062d18dbe2bdc2406877509c7ae5b8c
jcheron <myaddressmail@gmail.com>
2023-01-21 12:40:41 +0100

    re add methods generation

--- src/Ubiquity/views/engine/latte/LatteTemplateGenerator.php
+++ src/Ubiquity/views/engine/latte/LatteTemplateGenerator.php
@@ -2,6 +2,9 @@
 
 namespace Ubiquity\views\engine\latte;
 
+use Ubiquity\utils\base\UArray;
+use Ubiquity\utils\base\UString;
+
 /**
  * Latte template generator.
  * Ubiquity\views\engine\latte$LatteTemplateGenerator
@@ -13,6 +16,92 @@ namespace Ubiquity\views\engine\latte;
  */
 class LatteTemplateGenerator extends \Ubiquity\views\engine\TemplateGenerator {
 
+	public function __construct() {
+		$this->openExpressionTag = $this->openVarTag = '{';
+		$this->closeExpressionTag = $this->closeVarTag = '}';
+		$this->safeFilter = 'noescape';
+	}
+
+	public function openBlock(string $name): string {
+		return $this->openExpressionTag . "block $name" . $this->closeExpressionTag;
+	}
+
+	public function closeBlock(): string {
+		return $this->openExpressionTag . '/block' . $this->closeExpressionTag;
+	}
+
+	public function asArray(array $array): string {
+		return UArray::asPhpArray_($array);
+	}
+
+	public function insertVariable(string $name, bool $safe = false): string {
+		$filter = '';
+		if ($safe) {
+			$filter = '|' . $this->safeFilter;
+		}
+		$name = \str_replace('.', '->', $name);
+		if (UString::contains('(', $name)) {
+			return $this->openVarTag . $name . $filter . $this->closeVarTag;
+		}
+		return $this->openVarTag . $this->asVariable($name) . $filter . $this->closeVarTag;
+	}
+
+	public function asVariable(string $var): string {
+		return '$' . \ltrim($var, '$');
+	}
+
+	public function includeFile(string $filename, bool $asVariable = false): string {
+		$quote = "'";
+		if ($asVariable) {
+			$quote = '';
+			$filename = $this->asVariable($filename);
+		}
+		return $this->openExpressionTag . "include {$quote}{$filename}{$quote}" . $this->closeExpressionTag;
+	}
+
+	public function extendsTemplate(string $templateName, bool $asVariable = false): string {
+		$quote = "'";
+		if ($asVariable) {
+			$quote = '';
+			$templateName = $this->asVariable($templateName);
+		}
+		return $this->openExpressionTag . "layout {$quote}{$templateName}{$quote}" . $this->closeExpressionTag;
+	}
+
+	public function foreach(string $arrayName, string $value, ?string $key = null): string {
+		$arrayName = $this->asVariable($arrayName);
+		$value = $this->asVariable($value);
+		if ($key != null) {
+			$key = $this->asVariable($key);
+			return $this->openExpressionTag . "foreach $arrayName as $key=>$value" . $this->closeExpressionTag;
+		}
+		return $this->openExpressionTag . "foreach $arrayName as $value" . $this->closeExpressionTag;
+	}
+
+	public function endForeach(): string {
+		return $this->openExpressionTag . '/foreach' . $this->closeExpressionTag;
+	}
+
+	public function condition(string $condition): string {
+		$condition = $this->asVariable(\trim($condition));
+		return $this->openExpressionTag . "if $condition" . $this->closeExpressionTag;
+	}
+
+	public function endCondition(): string {
+		return $this->openExpressionTag . '/if' . $this->closeExpressionTag;
+	}
+
+	public function getNonce(): string {
+		return $this->openExpressionTag . "\$nonce??''" . $this->closeExpressionTag;
+	}
+
+	public function getNonceArray(): string {
+		return "['nonce'=>\$nonce??'']";
+	}
+
+	public function getSelf(): string {
+		return $this->openExpressionTag . '$this->getName()' . $this->closeExpressionTag;
+	}
 
 	private function postProcess(string $code): string {
 		return \str_replace('$nonce', '$nonce??""', $code);
